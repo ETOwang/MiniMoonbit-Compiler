1dc87357db4bdddd0d697d526e08f8aec3f0ae09
Wang chenyang <221250060@smail.nju.edu.cn>
2024-11-09 18:18:25 +0800

    delete .data and .text

--- src/riscv/output.s
+++ src/riscv/output.s
@@ -1,66 +1,30 @@
+.data:
+
+.text:
 .global minimbt_main
 minimbt_main:
-  addi sp, sp, -104
-  li t0, 12.3
+  addi sp, sp, -24
+  li t0, 123
   sw t0, 0(sp)
-  fld ft0, 0(sp)
-  fneg.d ft0, ft0
-  fmv.d.x ft0, t0
-  sw t0, 0(sp)
-  fld fa0, 0(sp)
-  call minimbt_abs_float
-  sw a0, 0(sp)
-  fld fa0, 0(sp)
-  call minimbt_sqrt
-  sw a0, 0(sp)
-  fld fa0, 0(sp)
-  call minimbt_cos
-  sw a0, 0(sp)
-  fld fa0, 0(sp)
-  call minimbt_sin
-  sw a0, 0(sp)
-  li t0, 4.5
-  sw t0, 0(sp)
-  fld ft0, 0(sp)
-  fld ft1, 0(sp)
-  fadd.d ft0, ft0, ft1
-  fmv.d.x ft0, t0
-  sw t0, 0(sp)
-  li t0, 6.7
-  sw t0, 0(sp)
-  li t0, 8.9
-  sw t0, 0(sp)
-  fld ft0, 0(sp)
-  fld ft1, 0(sp)
-  fmul.d ft0, ft0, ft1
-  fmv.d.x ft0, t0
-  sw t0, 0(sp)
-  li t0, 1.23456789
-  sw t0, 0(sp)
-  fld ft0, 0(sp)
-  fld ft1, 0(sp)
-  fdiv.d ft0, ft0, ft1
-  fmv.d.x ft0, t0
-  sw t0, 0(sp)
-  fld ft0, 0(sp)
-  fld ft1, 0(sp)
-  fsub.d ft0, ft0, ft1
-  fmv.d.x ft0, t0
-  sw t0, 0(sp)
-  li t0, 1000000
-  sw t0, 0(sp)
-  ld a0, 0(sp)
-  call minimbt_float_of_int
-  sw a0, 0(sp)
-  fld ft0, 0(sp)
-  fld ft1, 0(sp)
-  fmul.d ft0, ft0, ft1
-  fmv.d.x ft0, t0
-  sw t0, 0(sp)
-  fld fa0, 0(sp)
-  call minimbt_int_of_float
-  sw a0, 0(sp)
-  ld a0, 0(sp)
+  lw a0, 0(sp)
   call minimbt_print_int
-  addi sp, sp, 104
+  li t1, 456
+  sw t1, 8(sp)
+  lw t2, 8(sp)
+  neg t2, t2
+  sw t2, 4(sp)
+  lw a0, 4(sp)
+  call minimbt_print_int
+  li t3, 789
+  sw t3, 16(sp)
+  li t4, 0
+  sw t4, 20(sp)
+  lw t5, 16(sp)
+  lw t0, 20(sp)
+  add t5, t5, t0
+  sw t5, 12(sp)
+  lw a0, 12(sp)
+  call minimbt_print_int
+  addi sp, sp, 24
+  ret
 
