355338563128ba6fff3a769761443604f80e6e54
221250060-Wang Chenyang <221250060@smail.nju.edu.cn>
2024-11-10 14:25:03 +0800

    add a test script

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if input argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <input>"
+    exit 1
+fi
+
+INPUT=$1
+
+# Run moonrun with the specified input
+moonrun target/wasm-gc/release/build/bin/bin.wasm -- test/test_src/${INPUT}.mbt -o test/test_src/${INPUT}.s
+
+cat test/test_src/${INPUT}.s
+# Build the executable using zig
+zig build-exe -target riscv64-linux -femit-bin=./${INPUT}.out ./test/test_src/${INPUT}.s riscv_rt/zig-out/lib/libmincaml.a -O Debug -fno-strip -mcpu=baseline_rv64
+
+
+# Run the executable using qemu-riscv64
+qemu-riscv64 ./${INPUT}.out
