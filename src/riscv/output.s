c0bf72d3dd17760373989cb485ca94b4f6606f0f
221250060-Wang Chenyang <221250060@smail.nju.edu.cn>
2024-11-13 12:25:49 +0800

    merge all

--- src/riscv/output.s
+++ src/riscv/output.s
@@ -1,30 +1,6 @@
-.data:
+function main() {
+minimbt_print_int(minimbt_int_of_float((((minimbt_sin(minimbt_cos(minimbt_sqrt(minimbt_abs_float(-12.3)))) + 4.5) - ((6.7 * 8.9) / 1.23456789)) * minimbt_float_of_int(1000000))));
+minimbt_print_endline();
+}
 
-.text:
-.global minimbt_main
-minimbt_main:
-  addi sp, sp, -24
-  li t0, 123
-  sw t0, 0(sp)
-  lw a0, 0(sp)
-  call minimbt_print_int
-  li t1, 456
-  sw t1, 8(sp)
-  lw t2, 8(sp)
-  neg t2, t2
-  sw t2, 4(sp)
-  lw a0, 4(sp)
-  call minimbt_print_int
-  li t3, 789
-  sw t3, 16(sp)
-  li t4, 0
-  sw t4, 20(sp)
-  lw t5, 16(sp)
-  lw t0, 20(sp)
-  add t5, t5, t0
-  sw t5, 12(sp)
-  lw a0, 12(sp)
-  call minimbt_print_int
-  addi sp, sp, 24
-  ret
-
+export default main;
